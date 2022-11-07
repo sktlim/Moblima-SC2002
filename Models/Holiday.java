5507ff0c5f21922ce0e3d26b0ca776ac4fc682e8
Wayne Tan <wtjh00@gmail.com>
2022-11-08 00:41:30 +0800

    refactored Holiday model class

--- Models/Holiday.java
+++ Models/Holiday.java
@@ -3,10 +3,20 @@ import java.time.LocalDate;
 
 public class Holiday {
 
+    private String name;
     private String date;
 
-    public Holiday(String date) {
+    public Holiday(String name, String date) {
+        this.name = name;
         this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDate() {
