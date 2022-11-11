2744fa6d963b5bcb86543643eaa808f31076ab57
sktlim <suikiat00@gmail.com>
2022-11-11 12:47:29 +0800

    Updated javadoc comments for models package

--- Models/Theatre.java
+++ Models/Theatre.java
@@ -1,19 +1,32 @@
 package Models;
 
+/**
+ * Theatre associated to Cineplex. Contains theatre ID, Cineplex code and Theatre Class
+ */
 public class Theatre {
-
+    /**
+     * Theatre ID of particular theatre
+     */
     private int theatreId;
+    /**
+     * Cineplex code of associated Theatre
+     */
     private String cineplexCode;
+    /**
+     * Enumerated variables for Theatre Classes. Consists of STANDARD, PLATINUM and DEFAULT
+     */
     private Show.TheatreClass theatreClass;
-
+    /**
+     * Status of whether the theatre i.e. whether it is showing movie currently
+     */
     private int isBusy;
 
     /**
      * Constructor for Theatre
-     * @param theatreId
-     * @param cineplexCode
-     * @param theatreClass
-     * @param isBusy
+     * @param theatreId Theatre ID of particular theatre
+     * @param cineplexCode Cineplex code of associated Theatre
+     * @param theatreClass Enumerated variables for Theatre Classes. Consists of STANDARD, PLATINUM and DEFAULT
+     * @param isBusy Status of whether the theatre i.e. whether it is showing movie currently
      */
     public Theatre(int theatreId, String cineplexCode, Show.TheatreClass theatreClass, int isBusy) {
         this.theatreId = theatreId;
@@ -23,34 +36,62 @@ public class Theatre {
     }
 
     /**
-     * Getter and Setter Methods
-     * @return
+     * Getter method for theatre ID
+     * @return returns Theatre ID of particular theatre
      */
     public int getTheatreId() {
         return theatreId;
     }
 
+    /**
+     * Setter method for theatre ID
+     * @param theatreId  New Theatre ID of particular theatre
+     */
     public void setTheatreId(int theatreId) {
         this.theatreId = theatreId;
     }
 
+    /**
+     * Getter method for Cineplex code
+     * @return returns Cineplex code of theatre
+     */
     public String getCineplexCode() {
         return cineplexCode;
     }
 
+    /**
+     * Getter method for cinema status
+     * @return return status of cinema. 1 represents busy and 0 represents not busy
+     */
     public int getStatus(){return isBusy;}
 
+    /**
+     * Setter method for Cineplex code
+     * @param cineplexCode Cineplex code in Integer format
+     */
     public void setCineplexCode(String cineplexCode) {
         this.cineplexCode = cineplexCode;
     }
 
+    /**
+     * Getter method for Cineplex code
+     * @return returns theatre class in enumerated format. Consists of STANDARD, PLATINUM and DEFAULT
+     */
     public Show.TheatreClass getTheatreClass() {
         return theatreClass;
     }
 
+    /**
+     * Setter method for theatre class code
+     * @param theatreClass theatre class in enumerated format. Consists of STANDARD, PLATINUM and DEFAULT
+     */
     public void setTheatreClass(Show.TheatreClass theatreClass) {
         this.theatreClass = theatreClass;
     }
 
+    /**
+     * Setter method for status of theatre
+     * @param isBusy status of cinema. 1 represents busy and 0 represents not busy
+     */
     public void setStatus(int isBusy){this.isBusy = isBusy;}
 }
--- Models/Transaction.java
+++ Models/Transaction.java
@@ -1,18 +1,32 @@
 package Models;
 
+/**
+ * Transaction class consisting of Transaction ID, ticket ID, movie goer ID, and datetime.
+ */
 public class Transaction {
-
+    /**
+     * Transaction ID of transaction
+     */
     private String TID;
+    /**
+     * Ticket ID associated to transaction
+     */
     private int ticketId;
+    /**
+     * Movie Goer ID associated to transaction
+     */
     private int movieGoerId;
+    /**
+     * Date time associated with transaction. Date time format is in YYYY-MM-DD;HH:MM
+     */
     private String dateTime;
 
     /**
      * Constructor for Transaction
-     * @param TID
-     * @param ticketId
-     * @param movieGoerId
-     * @param dateTime
+     * @param TID Transaction ID of transaction
+     * @param ticketId Ticket ID associated to transaction
+     * @param movieGoerId Movie Goer ID associated to transaction
+     * @param dateTime Date time associated with transaction. Date time format is in YYYY-MM-DD;HH:MM
      */
     public Transaction(String TID, int ticketId, int movieGoerId, String dateTime) {
         this.TID = TID;
@@ -22,37 +36,65 @@ public class Transaction {
     }
 
     /**
-     * Getter and Setter Methods
-     * @return
+     * Getter method for transaction ID
+     * @return returns transaction ID of particular transaction
      */
     public String getTID() {
         return TID;
     }
 
+    /**
+     * Setter method for transaction ID
+     * @param TID new transaction ID of particular transaction
+     */
     public void setTID(String TID) {
         this.TID = TID;
     }
 
+    /**
+     * Getter method for ticket ID
+     * @return returns ticket ID associated to transaction
+     */
     public int getTicketId() {
         return ticketId;
     }
 
+    /**
+     * Setter method for ticket ID
+     * @param ticketId new ticket ID associated to transaction
+     */
     public void setTicketId(int ticketId) {
         this.ticketId = ticketId;
     }
 
+    /**
+     * Getter method for Movie Goer ID
+     * @return returns Movie Goer ID associated to transaction
+     */
     public int getMovieGoerId() {
         return movieGoerId;
     }
 
+    /**
+     * Setter method for Movie Goer ID
+     * @param movieGoerId new Movie Goer ID associated to transaction
+     */
     public void setMovieGoerId(int movieGoerId) {
         this.movieGoerId = movieGoerId;
     }
 
+    /**
+     * Getter method for date time
+     * @return returns date time in YYYY-MM-DD;HH:MM format
+     */
     public String getDateTime() {
         return dateTime;
     }
 
+    /**
+     * Setter method for date time
+     * @return new date time in YYYY-MM-DD;HH:MM format
+     */
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
